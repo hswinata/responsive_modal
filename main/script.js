43d05fe7698a919d66353058f4eee214670daee6
hswinata <harrysurywinata@gmail.com>
2024-04-07 12:42:36 +0200

    Add slide function

--- main/script.js
+++ main/script.js
@@ -1,9 +1,51 @@
 const prev = document.querySelector(".prev");
 const next = document.querySelector(".next");
-const slideOneDot = document.getElementById("js-slide-one");
-const slideTwoDot = document.getElementById("js-slide-two");
-
 const slides = document.querySelectorAll(".modal__slide");
 const dots = document.querySelectorAll(".modal__navigation-dot");
 
-prev.addEventListener("click", () => {});
+//Slide functions
+function deactivateSlide() {
+  const activeSlide = document.querySelector(".modal__slide[data-active]");
+  if (activeSlide) {
+    activeSlide.removeAttribute("data-active");
+  }
+}
+
+function activateNextSlide() {
+  const activeIndex = Array.from(slides).findIndex((slide) =>
+    slide.hasAttribute("data-active")
+  );
+  deactivateSlide();
+  const nextIndex = (activeIndex + 1) % slides.length;
+  slides[nextIndex].setAttribute("data-active", "");
+}
+
+function activatePrevSlide() {
+  const activeIndex = Array.from(slides).findIndex((slide) =>
+    slide.hasAttribute("data-active")
+  );
+  deactivateSlide();
+  const prevIndex = (activeIndex - 1 + slides.length) % slides.length;
+  slides[prevIndex].setAttribute("data-active", "");
+}
+
+//Dot functions
+function deactivateDot() {
+  const activeDot = document.querySelector(
+    ".modal__navigation-dot[data-active]"
+  );
+  if (activeDot) {
+    activeDot.removeAttribute("data-active");
+  }
+}
+
+function activateNextDot() {
+  const activeIndex = Array.from(dots).findIndex((dot) => {
+    dot.hasAttribute("data-active");
+  });
+  deactivateDot();
+  const nextIndex = activeIndex + 1;
+}
+
+next.addEventListener("click", activateNextSlide);
+prev.addEventListener("click", activatePrevSlide);
