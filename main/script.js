2157b4bc7bc6bcc6c8c654a25b3c154d70760e84
hswinata <harrysurywinata@gmail.com>
2024-04-07 11:39:21 +0200

    Add 2nd slide

--- main/script.js
+++ main/script.js
@@ -1,1 +1,9 @@
-const = document.getElementById();
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const slideOneDot = document.getElementById("js-slide-one");
+const slideTwoDot = document.getElementById("js-slide-two");
+
+const slides = document.querySelectorAll(".modal__slide");
+const dots = document.querySelectorAll(".modal__navigation-dot");
+
+prev.addEventListener("click", () => {});
