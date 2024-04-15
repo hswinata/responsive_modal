41ead85c7b419817efe1391e78021c3bc8e3e089
hswinata <harrysurywinata@gmail.com>
2024-04-15 14:50:35 +0200

    Added swipe function

---
+++ js/touch.js
@@ -0,0 +1,51 @@
+import { activateNextSlide, activatePrevSlide } from "./slide.js";
+import { activateNextDot, activatePrevDot } from "./dot.js";
+
+const treshhold = 50;
+let startX = 0;
+let startY = 0;
+let distX = 0;
+let distY = 0;
+
+export function handleTouchStart(e) {
+  //Get the touch position relative to the viewport
+  const touch = e.touches[0];
+
+  startX = touch.clientX;
+  startY = touch.clientY;
+}
+
+export function handleTouchMove(e) {
+  e.preventDefault();
+
+  const touch = e.touches[0];
+  const currentX = touch.clientX;
+  const currentY = touch.clientY;
+
+  //Calculate the distance moved horizontally and vertically
+  distX = currentX - startX;
+  distY = currentY - startY;
+}
+
+export function handleTouchEnd(e) {
+  const absDistX = Math.abs(distX);
+  const absDistY = Math.abs(distY);
+
+  //Only activate swipe when absDistX is bigger than 50 to avoid accidental swipes
+  if (absDistX > treshhold) {
+    //Horizontal swipe gesture detected
+    if (absDistX > absDistY) {
+      if (distX > 0) {
+        //swipeRight
+        activateNextSlide();
+        activateNextDot();
+        console.log("Swipe left", absDistX);
+      } else {
+        //swipeLeft
+        activatePrevSlide();
+        activatePrevDot();
+        console.log("Swipe right", absDistX);
+      }
+    }
+  }
+}
--- script.js
+++ script.js
@@ -11,10 +11,23 @@ import {
   activateDotByIndex,
 } from "./js/dot.js";
 
+import {
+  handleTouchStart,
+  handleTouchMove,
+  handleTouchEnd,
+} from "./js/touch.js";
+
+const modal = document.querySelector(".modal");
 const closeButtons = document.querySelectorAll(".modal__close");
 const prevButton = document.querySelector(".prev");
 const nextButton = document.querySelector(".next");
 
+//Swipe functions
+modal.addEventListener("touchstart", handleTouchStart);
+modal.addEventListener("touchmove", handleTouchMove);
+modal.addEventListener("touchend", handleTouchEnd);
+
+//Prev and next functions
 nextButton.addEventListener("click", () => {
   activateNextDot();
   activateNextSlide();
@@ -24,7 +37,8 @@ prevButton.addEventListener("click", () => {
   activatePrevDot();
   activatePrevSlide();
 });
-g
+
+//Click dot functions
 dots.forEach((dot, index) => {
   dot.addEventListener("click", () => {
     activateDotByIndex(index);
